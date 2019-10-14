5cf032aabd097e413163a456f709e6d5685c2cf5
James Orwell <james@kingston.ac.uk>
2019-10-14 17:39:20 +0100

    initial commit

---
+++ src/main/java/ku/piii2019/worksheet4/MediaItem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ku.piii2019.worksheet4;
+
+/**
+ *
+ * @author James
+ */
+public class MediaItem {
+
+    private String absolutePath;
+
+    public String getAbsolutePath() {
+        return absolutePath;
+    }
+
+    public MediaItem setAbsolutePath(String absolutePath) {
+        this.absolutePath = absolutePath;
+        return this;
+    }
+
+}
