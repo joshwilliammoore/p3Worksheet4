514df4ec4a9d3aeacebf4ca4edc3ef76e83fe15c
Joshua Moore <K1720552@kingston.ac.uk>
2019-11-06 12:21:24 +0000

    HashCode() and Equals() methods added to MediaItem class.

--- src/main/java/ku/piii2019/worksheet4/MediaItem.java
+++ src/main/java/ku/piii2019/worksheet4/MediaItem.java
@@ -5,11 +5,17 @@
  */
 package ku.piii2019.worksheet4;
 
+import java.io.*;
+import static java.nio.file.FileVisitResult.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.Objects;
+
 /**
  *
  * @author James
  */
-public class MediaItem {
+public class MediaItem extends SimpleFileVisitor<Path>{
 
     private String absolutePath;
 
@@ -22,4 +28,30 @@ public class MediaItem {
         return this;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.absolutePath);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MediaItem other = (MediaItem) obj;
+        if (!Objects.equals(this.absolutePath, other.absolutePath)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
